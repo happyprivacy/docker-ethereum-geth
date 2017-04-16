8a87ca5f6c80d1ada641ab3dbc1c51e016ee6882
Alexandre Roba <alexandre.roba@arobanet.be>
2017-04-16 15:57:53 +0200

    Completed the devnet settings

--- ethereum-geth-devnet/buildgenesis.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-coinbase=$(geth  --datadir . --dev --rpc --rpcaddr 0.0.0.0 --exec eth.coinbase console)
-echo "Coinbase: $coinbase"
-sed -i s/\"ADDRESS\"/$coinbase/g config/genesis.json
-echo "Coinbase written in genesis.json file."
